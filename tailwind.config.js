639623d87987dceaeb234ca0ab035dcca5e17e33
Rjmhrzn-002 <rjmhrzn.143@gmail.com>
2023-11-22 14:04:29 +0545

    tailwindcss first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        customBlue: "#4A1B73",
+        customPurple: "#455AA5",
+      },
+    },
+  },
+  plugins: [],
+};
